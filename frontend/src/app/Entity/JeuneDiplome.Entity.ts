c0d590a78eb7aa09f382c7eb524bb1f499ad90a2
khalil mabrouki <khalil.mabrouki@issatm.ucar.tn>
2026-08-24 11:49:24 +0100

    add

---
+++ frontend/src/app/Entity/JeuneDiplome.Entity.ts
@@ -0,0 +1,16 @@
+export class JeuneDiplome {
+
+constructor(
+    public id?: number,
+    public nom?: string,
+    public prenom?: string,
+    public email?: string,
+    public mdp?: string,
+    public telephone?: string,
+    public cv?: string,
+    public diplome?: string,
+    public specialite?: string,
+    public anneesExperience?: number,
+    public statut?: string
+  ) {}
+}
---
+++ frontend/src/app/Entity/ResponsableEntreprise.Entity.ts
@@ -0,0 +1,21 @@
+export class ResponsableEntreprise {
+
+
+  constructor(
+
+
+
+    public id?: number,
+    public nom?: string,
+    public prenom?: string,
+    public email?: string,
+    public mdp?: string,
+    public telephone?: string,
+    public nomEntreprise?: string,
+    public adresseEntreprise?: string,
+    public poste?: string,
+    public statut?: string
+
+    
+  ) {}
+}
